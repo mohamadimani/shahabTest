90f18d86e929a5bfa8197d97c3a2399934dba9ad
manimohamadi <mohamadimahdi@yahoo.com>
2024-03-01 11:27:54 +0330

    up++

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('orders', [OrderController::class, 'index']);
-Route::post('orders', [OrderController::class, 'index']);
+Route::post('orders', [OrderController::class, 'store']);
